3b738507f6e9dd335b5bd35418ec6263109b0827
RednaxMarc <xander.marcle@hotmail.com>
2023-12-05 12:07:38 +0100

    Completed day.1.1

--- Day1/day1.1.py
+++ Day1/day1.1.py
@@ -7,21 +7,21 @@ with open(file_path, 'r') as file:
     for line in file:
         lines_list.append(line.strip())  # Use strip() to remove trailing newline characters or extra spaces
 
-stringNum = []
-revstringNum = []
+firstDigit = []
+lastDigit = []
 intNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 for string in lines_list: # Check for the first number in the string and add it to a list
     for i in string:
         if i in intNumbers:
-            stringNum.append(i)
+            firstDigit.append(i)
             break
     for i in string[::-1]: # Check for the last number in the sting and add it to a list
         if i in intNumbers:
-            revstringNum.append(i)
+            lastDigit.append(i)
             break
 
 # Zip the two lists and concatenate their values
-stringResult = [str(item1) + str(item2) for item1, item2 in zip(stringNum, revstringNum)]
+stringResult = [str(item1) + str(item2) for item1, item2 in zip(firstDigit, lastDigit)]
 
 # Convert everything to an integer
 finalResult = []
