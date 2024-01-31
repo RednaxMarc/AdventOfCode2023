1ac43f07f22726d121aac553fa992219438fadb5
RednaxMarc <xander.marcle@hotmail.com>
2024-02-01 00:00:11 +0100

    finished day 6

---
+++ Day6/day6.1.py
@@ -0,0 +1,34 @@
+# Problem 1
+with open('input.txt') as file: 
+    lines = file.readlines()
+    times = [int(x) for x in lines[0].strip().split(' ')[1:] if x!='']
+    distances = [int(x) for x in lines[1].strip().split(' ')[1:] if x!='']
+    print(times)
+    print(distances)
+
+    possibilites = 1
+    for i, time in enumerate(times):
+        distance = distances[i]
+        wins=0
+        for j in range(1, time+1):
+            d = (time-j)*j
+            if d > distance:
+                wins+=1
+        possibilites *= wins
+    print(possibilites)
+file.close() 
+
+# Problem 2
+with open('input.txt') as file: 
+    lines = file.readlines()
+    time = int(''.join([x for x in lines[0].strip().split(' ')[1:] if x!='']))
+    distance = int(''.join([x for x in lines[1].strip().split(' ')[1:] if x!='']))
+    print(time)
+    print(distance)
+    wins = 0
+    for j in range(1, time+1):
+        d = (time-j)*j
+        if d > distance:
+            wins+=1
+    print(wins)
+    
