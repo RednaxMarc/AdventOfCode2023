d1a24ce2f1c1af553b38fed6b2dd23446ff81495
RednaxMarc <xander.marcle@hotmail.com>
2024-02-06 17:07:39 +0100

    Working on day8.1.py

--- Day8/day8.1.py
+++ Day8/day8.1.py
@@ -1,3 +1,22 @@
+import re
+
 with open('test.txt') as file:
-    instructions = file.readlines()[0].strip()
+    lines = file.readlines()
+    instructions = [char.replace('L', '0').replace('R', '1') for char in lines[0].strip()]
+    print(instructions)
+    nodes = {}
+    for line in lines[2:]:
+        nodes[line[0:3]]= [line[7:10],line[12:15]]
+    print(nodes)
+   
+    k = 'AAA'
+    steps = 0
+    
+    for i in instructions:
+        while k != 'ZZZ':
+            print(k)
+            k = nodes[k][int(i)]
+
+
+
     
