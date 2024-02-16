e713ed7516fb146e224938cf3df04e346825ab5a
RednaxMarc <xander.marcle@hotmail.com>
2024-02-16 09:25:09 +0100

    Finished part 2

--- Day9/day9.2.py
+++ Day9/day9.2.py
@@ -1,9 +1,9 @@
 sum = 0
-with open("test.txt") as file:
+with open("input.txt") as file:
     for line in file.readlines():
         values = {}
         values[0] = [int(x) for x in line.strip().split(" ")]
-        print(values)
+        # print(values)
 
         # Using a loop to calculate the differences between the values until they are all equal
         n = 1
@@ -13,3 +13,14 @@ with open("test.txt") as file:
             are_all_equal = all(x == values[n][0] for x in values[n][1:])
             n += 1
         # print(values)
+            
+        # Duplicate the a value of the last key:value pair
+        values[len(values)-1].append(values[len(values)-1][-1])
+        # print(values)
+
+        # Extrapolating like the example online
+        for x in range(len(values)-1 , 0, -1):
+            values[x-1].insert(0, values[x-1][0] - values[x][0])
+        print(values)
+        sum += values[0][0]
+    print(sum)
