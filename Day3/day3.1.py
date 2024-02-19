6e604a1cd8d9cb2df0a70ad36da04d4df5b76ac6
RednaxMarc <xander.marcle@hotmail.com>
2024-02-19 21:51:24 +0100

    ???

--- Day3/day3.1.py
+++ Day3/day3.1.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+import re
 
-# Open the file in read mode
-file_path = 'D:\Documents\Howest\Linux\SF\Fedora\AdventOfCode2023\Day3\input.txt'
+with open('test.txt') as file:
+    grid = [row.strip() for row in file.readlines()]
+    print(grid)
 
-# Read each line of the file
-with open(file_path, 'r') as file:
-    for line in file.readlines():
-        # Strip the line of whitespace
-        line = line.strip()
-        print(line)
+            
+   
+
+
+        
+
+
+    
+    
