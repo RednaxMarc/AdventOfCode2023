49cc629235120d546c619417d684556709cd999f
RednaxMarc <xander.marcle@hotmail.com>
2024-10-21 14:34:50 +0200

    busy on 10

--- Day10/day10.1.py
+++ Day10/day10.1.py
@@ -3,22 +3,36 @@ import numpy as np
 ## Read in the file
 with open("test1.txt") as file:
     data = np.array([list(line.strip()) for line in file])
-    print(data[1,1])
+    print(data)
     
     # Find the "S" in the data
     start = np.where(data == "S")
     print(start)
     print(data[start])
+    current = start
 
     ## Define the directions
-    # "|": up or down, so y +1  or y -1
-    # "-": left or right, so x +1 or x -1
-    # "L": down and right, so y +1 and x +1, or left and up, so x -1 and y -1
-    # "J": down and left, so y +1 and x -1, or right and up, so x +1 and y -1
-    # "7": up and left, so y -1 and x -1, or right and down, so x +1 and y +1
-    # "F": up and right, so y -1 and x +1, or left and down, so x -1 and y +1
+    # "|": N, or opposite
+    # "-": E, or opposite
+    # "L": SE, or opposite
+    # "J": SW, or opposite
+    # "7": ES, or opposite
+    # "F": NE, or opposite
     # ".": no movement
     # "S": start
+
+    opposite = {
+        "N": "S",
+        "S": "N",
+        "E": "W",
+        "W": "E",
+    }
+    allowed_directions = 
+
+
+    
+
+
 
     
 
